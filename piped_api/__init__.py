56d70e7230c4d51a2243b85e98cb246c23c5a3b4
Kevo <me@kevo.link>
2022-02-27 08:43:50 +0100

    Fix setup.py

--- piped_api/__init__.py
+++ piped_api/__init__.py
@@ -1,13 +1,23 @@
 import typing as t
 
+from pathlib import Path
 
 from .client import PipedClient
-
 from .models.comments import Comments
 
 
-from setup import __doc__ as __sdoc__
-__doc__ = __sdoc__
+
+# For pdoc:
+README_PATH = Path(__file__).parent.absolute() / Path('README.md')
+try:
+    with open(README_PATH, 'r', encoding="UTF-8") as readme:
+        __readme__ = readme.read()
+
+except:
+    __readme__ = "Failed to read README.md!"
+
+__doc__ = __readme__
+
 
 
 # Supress unused-import warnings:
